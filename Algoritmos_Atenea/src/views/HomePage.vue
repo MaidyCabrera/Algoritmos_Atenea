12b6cdb3a826217851bd3799e810d183209cb2c2
Maidy Cabrera <maidyc914@gmail.com>
2023-03-26 17:15:28 -0500

    Upadte: Update Page

--- Algoritmos_Atenea/src/views/HomePage.vue
+++ Algoritmos_Atenea/src/views/HomePage.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <h1>Mi Primera Vista en Vue</h1>
+    <h1>Ejercicios y Algoritmos Atenea</h1>
   </div>
-  <router-link to="/ejercicios">Ejercicios</router-link>
+  <router-link to="/ejercicios">Algotitmos Js, HTML & CSS</router-link>
   <br>
   <a href="https://maidycabrera.github.io/Calculadora/">Calculadora</a>
+  <br>
+  <br>
+  <a href="https://github.com/MaidyCabrera/Cronometro">Repo Cronómetro</a>
+  <a href="https://maidycabrera.github.io/Cronometro/">Page Cronometro</a>
+  <br>
+  <a href="https://github.com/MaidyCabrera/Algoritmos_Atenea/tree/main/Formulario_HTML">Repo Formulario HTML</a>
+  <br>
+  <a href="https://github.com/MaidyCabrera/Algoritmos_Atenea/tree/main/Formulario_HTML">Repo Local Storage</a>
+  <br>
+  <br>
+  <br>
   <br>
   <img src="https://media2.giphy.com/media/fYYdzgrYnlwch00xUN/giphy.gif?cid=790b7611a6f4d0f1e9a4f9ac0729cad2e072b2c59fbe1a2f&rid=giphy.gif&ct=s">
   
